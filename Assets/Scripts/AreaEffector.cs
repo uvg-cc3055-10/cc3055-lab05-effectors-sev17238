146a04a5899a743ea95c6b9b23c8f9454d9d281f
DESKTOP-1F2LFR5\SDiego <sev17238@uvg.edu.gt>
2018-05-02 17:28:50 -0600

    Laboratorio_5 - Joints, Particles y Effectors Terminado!!!

---
+++ Assets/Scripts/AreaEffector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaEffector : MonoBehaviour {
+
+    private float time = 0;
+    public GameObject go;
+ 
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time = time + Time.deltaTime;
+        go.SetActive(true);
+
+        if (time > 3) { //Cuando el tiempo pasa de 3 segundos, se desactiva la cascada.
+            go.SetActive(false);
+            time = 0;
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Weight.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weight : MonoBehaviour {
+    public DistanceJoint2D distJoint;
+    public LineRenderer line;
+	// Use this for initialization
+	void Start () {
+        distJoint = GetComponent<DistanceJoint2D>();
+        line = GetComponent<LineRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        line.SetPosition(0, transform.position); //se coloca la posicion inicial del objeto
+        //se coloca la posicion final del objeto haciendo referencia al peso que esta conectado
+        line.SetPosition(1,distJoint.connectedBody.transform.position); 
+    }
+}
